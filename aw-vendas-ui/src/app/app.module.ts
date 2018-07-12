5958c6664f2ddd78bfa2969cb02d52a3fdb020e0
Rafael <raphab33@hotmail.com>
2018-07-12 06:49:47 -0300

    Criado component Vendas-listagem e serviço Vendas

--- aw-vendas-ui/src/app/app.module.ts
+++ aw-vendas-ui/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { TableModule } from 'primeng/table';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { VendasListagemComponent } from './vendas-listagem/vendas-listagem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    VendasListagemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ aw-vendas-ui/src/app/vendas-listagem/vendas-listagem.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { VendasService } from '../vendas/vendas.service';
+
+@Component({
+  selector: 'app-vendas-listagem',
+  templateUrl: './vendas-listagem.component.html',
+  styleUrls: ['./vendas-listagem.component.css']
+})
+export class VendasListagemComponent implements OnInit {
+
+  vendas: Array<any>;
+  constructor(private vendaService: VendasService) { }
+
+  ngOnInit() {
+    this.vendaService.listar()
+    .subscribe(Response => this.vendas = Response);
+  }
+
+}
